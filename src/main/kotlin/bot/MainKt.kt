6c0dc2e81b3e126982a040d8ec612ebd64dae0a8
ordon <ordon1313@gmail.com>
2015-12-25 15:21:56 +0100

    lets play

--- src/main/kotlin/bot/MainKt.kt
+++ src/main/kotlin/bot/MainKt.kt
@@ -7,27 +7,49 @@ import retrofit.GsonConverterFactory
 import retrofit.Retrofit
 import retrofit.RxJavaCallAdapterFactory
 import retrofit.http.GET
+import retrofit.http.Path
 import rx.Observable
 
 object MainKt {
 
+    val retrofit = Retrofit.Builder()
+            .baseUrl("http://89.73.67.164:46176/")
+            .client(OkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()))
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build()
+    val newGameApi = retrofit.create(NewGameApi::class.java)
+    val moveApi = retrofit.create(MoveApi::class.java)
+
     fun main() {
-        val retrofit = Retrofit.Builder()
-                .baseUrl("http://89.73.67.164:46176/")
-                .client(OkHttpClient())
-                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()))
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build()
-        retrofit.create(NewGame::class.java).call().subscribe {
-            println(it.id)
+        newGameApi.call().subscribe { doMove(it) }
+    }
+
+    fun doMove(it: Game): Unit {
+        println(it.score)
+        if (it.next == -1000) {
+            newGameApi.call().subscribe { doMove(it) }
+        } else {
+            moveApi.call(it.id, 0).subscribe { doMove(it) }
         }
     }
 
-    interface NewGame {
+    interface NewGameApi {
 
         @GET("/new_game")
         fun call(): Observable<Game>
     }
 
-    data class Game(val id: String)
+    interface MoveApi {
+
+        @GET("/move/{gameId}/{target}")
+        fun call(@Path("gameId") gameId: String, @Path("target") target: Int): Observable<Game>
+    }
+
+    data class Game(
+            val id: String,
+            val board: List<Int>,
+            val next: Int,
+            val round: Int,
+            val score: Int)
 }
